23212cd88feab6bc4825af7d0120e6ed31d15558
Prakhar <mprakhar07@gmail.com>
2025-10-04 13:58:59 +0530

    add

---
+++ dsa ques/prakhar'scp/dummycontest2/fairplay.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int t;
+  cin >> t;
+  while (t--) {
+    vector<int> s(4);
+    for (int& x : s) cin >> x;
+    if (min(s[0], s[1]) > max(s[2], s[3]) || max(s[0], s[1]) < min(s[2], s[3]))
+      cout << "NO\n";
+    else
+      cout << "YES\n";
+  }
+}
+
+
+int main() {
+    solve();
+    return 0;
+}
