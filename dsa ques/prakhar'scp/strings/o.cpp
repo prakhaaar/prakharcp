0b3e5793cfddcb8eaa05b3c3301c66669f0c85db
Prakhar <mprakhar07@gmail.com>
2025-10-04 11:37:41 +0530

    o

---
+++ dsa ques/prakhar'scp/strings/o.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    for(int i = 0; i < n - 1; ++i) {
+        for(int j = 0; j < n - i - 1; ++j) {
+            if(s[j] > s[j + 1]) {
+                swap(s[j], s[j + 1]);
+            }
+        }
+    }
+    cout << s << "\n";
+}
+int main() {
+    solve();
+    return 0;
+}
