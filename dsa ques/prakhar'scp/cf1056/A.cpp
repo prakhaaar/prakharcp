23e8f76f6f12f620f06b9ee6cdfbdcbd5c799fb4
Prakhar <mprakhar07@gmail.com>
2025-10-06 03:51:12 +0530

    ,

---
+++ dsa ques/prakhar'scp/cf1056/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int n;
+    cin >> n;
+    cout << 2 * n - 2 << '\n';
+  }
+  re
