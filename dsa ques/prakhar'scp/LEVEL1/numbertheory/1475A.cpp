d1445720ceceb9266717f7a7375454faa513b9df
Prakhar <mprakhar07@gmail.com>
2025-10-10 13:23:09 +0530

    a.cpp

---
+++ dsa ques/prakhar'scp/LEVEL1/numbertheory/1475A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    fastio;
+    int n;
+    cin>>n;
+    ((n&(n-1))==0)?cout<<"NO\n":cout<<"YES\n";
+    
+
+    return 0;
+}
